49524a0ec6cdef63fc4d688721c617bc69f71334
Faris Imaduddin <faris.imad@hotmail.com>
2023-03-29 14:51:30 +0700

    NestJS Guards

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,35 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+  'jwt'
+) {
+  constructor(
+    config: ConfigService,
+    private primsa: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET')
+    });
+  }
+
+  async validate(payload: {
+    sub: number,
+    email: string;
+  }) {
+    const user = await this.primsa.user.findUnique({
+      where: {
+        id: payload.sub
+      }
+    });
+
+    delete user.hash;
+    return user;
+  }
+}
